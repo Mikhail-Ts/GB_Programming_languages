d3d8d087efe0abf30a1c475cbb1b08c4fa9b8989
Mikhail <tsybulkomk@yandex.ru>
2022-10-15 17:32:09 +0300

    Update Task_6

--- programming languages/Seminar_1/Task6/Program.cs
+++ programming languages/Seminar_1/Task6/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.Clear();
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+if (a%2 == 0)
+{
+    Console.Write("Число ");
+    Console.Write(a);
+    Console.Write(" четное");
+}
+else
+{
+    Console.Write("Число ");
+    Console.Write(a);
+    Console.Write(" нечетное");
+}
