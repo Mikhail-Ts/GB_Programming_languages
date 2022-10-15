03eca54885a61e986c735dfb842d175f10d0ebd3
Mikhail <tsybulkomk@yandex.ru>
2022-10-15 16:46:57 +0300

    create Task2

---
+++ programming languages/Seminar_1/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+if (a < b)
+{
+    Console.Write("min = ");
+    Console.WriteLine(a);
+    Console.Write("max = ");
+    Console.WriteLine(b);
+}
+else
+{
+    Console.Write("min = ");
+    Console.WriteLine(b);
+    Console.Write("max = ");
+    Console.WriteLine(a);
+}
