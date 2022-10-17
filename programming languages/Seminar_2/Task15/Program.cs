ed5f1b7024226e64fa978ff705a106f23c6fba3d
Mikhail <tsybulkomk@yandex.ru>
2022-10-17 19:49:14 +0300

    Create Task15

---
+++ programming languages/Seminar_2/Task15/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+Console.Write("Введите день недели (от 1 до 7): ");
+int day = int.Parse(Console.ReadLine());
+
+if(day<=5)
+{
+    Console.WriteLine("нет");
+}
+else if (day<=7)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("Некорректное число");
+}
