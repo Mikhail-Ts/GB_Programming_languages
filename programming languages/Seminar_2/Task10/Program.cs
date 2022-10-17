c347369e8e62692577b0aea69b6b09cc729dcfa2
Mikhail <tsybulkomk@yandex.ru>
2022-10-17 19:28:03 +0300

    Create Task10

---
+++ programming languages/Seminar_2/Task10/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int secondNumber = 0;
+if(number<=999 && number>=100)
+{
+    secondNumber = (number - ((number/100) * 100)) /10;
+    Console.WriteLine("Второе число: " + secondNumber);
+}
+else
+{
+    Console.WriteLine("Ведено не трехзначное число");
+};
+
+
