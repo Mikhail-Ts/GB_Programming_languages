2933262102ec73bfa2456711f6306191bfefcce0
Mikhail <tsybulkomk@yandex.ru>
2022-10-31 11:55:22 +0300

    Create DZ_6

---
+++ programming languages/Homework/ДЗ_6/Task41/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//            Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Clear();
+
+Console.Write("Введите числа через пробел: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+//0 = = 0;
+
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+
+}
+
+
+int CountPositiveNumbers(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+         if (array[i] > 0)
+         {
+             result ++;
+          }
+  }
+  return result ;
+}
+
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine($"Кол-во элементов > 0: " + (CountPositiveNumbers(array)));
