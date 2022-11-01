97925162dc1146567628e1f21f843558dfe3e07a
Mikhail <tsybulkomk@yandex.ru>
2022-11-01 18:33:50 +0300

    Update DZ_8 Task54

--- programming languages/Homework/ДЗ_8/Task54/Program.cs
+++ programming languages/Homework/ДЗ_8/Task54/Program.cs
@@ -1,4 +1,62 @@
 ﻿// Задача 54: Задайте двумерный массив. 
 //            Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
-
 Console.Clear();
+
+
+
+
+
+int[,] GetArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(0, 99);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+           Console.Write($"{array[i,j]} ");
+        }
+       Console.WriteLine();
+    } 
+    
+}
+
+
+void SortLineArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int x = 0; x < array.GetLength(1) - 1; x++)
+            {
+                if (array[i, x] < array[i, x + 1])
+                {
+                    int temp = array[i, x + 1];
+                    array[i, x + 1] = array[i, x];
+                    array[i, x] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] array = GetArray(3, 4); 
+Console.WriteLine("Изначальный массив:");
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine("Массив после обработки:");
+SortLineArray (array);
+PrintArray(array);
