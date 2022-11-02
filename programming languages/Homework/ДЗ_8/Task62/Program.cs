e5d5c2c82039584404508a16d523b21d62433503
Mikhail <tsybulkomk@yandex.ru>
2022-11-02 09:27:40 +0300

    Update DZ_8 Task62

--- programming languages/Homework/ДЗ_8/Task62/Program.cs
+++ programming languages/Homework/ДЗ_8/Task62/Program.cs
@@ -1,2 +1,38 @@
 ﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
 Console.Clear();
+
+void  CreateArray(int[,] array, int len)
+{
+    int i = 0, j = 0;
+    int value = 1;
+    for (int a = 0; a < len * len; a++)
+    {
+        int k = 0;
+        do { array[i, j++] = value++; } while (++k < len - 1);
+        for (k = 0; k < len - 1; k++) array[i++, j] = value++;
+        for (k = 0; k < len - 1; k++) array[i, j--] = value++;
+        for (k = 0; k < len - 1; k++) array[i--, j] = value++;
+        ++i; ++j;
+        if( len < 2) 
+            len = 0; 
+        else len = len - 2;
+        
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+           Console.Write($"{array[i,j]} ");
+        }
+       Console.WriteLine();
+    } 
+    
+}
+
+int[,] array = new int[4, 4];
+CreateArray(array, array.GetLength(0));
+PrintArray(array);
